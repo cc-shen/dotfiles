4799ca4f7ac8b00327c606787c5d7be765873024
Charles Shen <8899894+cc-shen@users.noreply.github.com>
2025-01-11 12:51:47 -0500

    lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,2 @@
--- No netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- No tohtml.vim
-vim.g.loaded_2html_plugin = 1
--- No plugins to check compressed files
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_tarPlugin = 1
-
-require('plugins')
-require('basics')
-require('keybinds')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
